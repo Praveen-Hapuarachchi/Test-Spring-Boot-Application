9b796fa924845979151b3b9550fa11eb5510eef1
Praveen-Hapuarachchi <hapup14@gmail.com>
2024-08-12 16:34:55 +0530

    second commit

--- Test-Spring-Boot-Application/src/main/java/com/ruhuna/Test/Spring/Boot/Application/controller/UserController.java
+++ Test-Spring-Boot-Application/src/main/java/com/ruhuna/Test/Spring/Boot/Application/controller/UserController.java
@@ -1,26 +1,45 @@
 package com.ruhuna.Test.Spring.Boot.Application.controller;
 
+import com.ruhuna.Test.Spring.Boot.Application.dto.UserDTO;
+import com.ruhuna.Test.Spring.Boot.Application.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "Simple-Root";
+    @Autowired
+    private UserService userService;
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
     }
     @PostMapping("/saveUser")
-    public  String saveUser(){
-        return  "User Saved";
+    public  UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+
     }
     @PutMapping("/updateUser")
-    public String updateUser(){
-        return  "User Update";
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
     }
     @DeleteMapping("/deleteUser")
-    public String deleteUser(){
-        return "User Delete!";
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+    @GetMapping("/getUserByUserId/{userId}")
+    public UserDTO getUserByUserID(@PathVariable String userId){
+        return userService.getUserByUserID(userId);
+    }
+    
+    @GetMapping("/getUserByUserIdAndAddress/{userId}/{address}")
+    public UserDTO getUserByUserIdAndAddress(@PathVariable String userId ,@PathVariable String address){
+        System.out.println("ÜserId : " + userId +" Address : " + address);
+        return userService.getUserByUserIdAndAddress(userId ,address);
     }
 }
 
